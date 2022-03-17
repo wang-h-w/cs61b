58976114c5029d2b03b66fe9a398300620752c6b
Haowen Wang <wang.h.w@outlook.com>
2022-03-17 11:45:43 +0800

    complete lab11 part 2: cycle check

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,50 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int s;
+    private boolean circle = false;
+    private int[] link;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(1, 1);
+        link = new int[maze.V()];
+        link[s] = s;  // replace edgeTo[]
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int w: maze.adj(v)) {
+            if (marked[w] && w != link[v]) {
+                circle = true;
+                link[w] = v;
+                int curr = v;
+                edgeTo[curr] = link[curr];
+                while (curr != w) {
+                    curr = link[curr];
+                    edgeTo[curr] = link[curr];
+                }
+                announce();
+                return;
+            }
+            if (!marked[w]) {
+                link[w] = v;
+                dfs(w);
+                if (circle) {
+                    return;
+                }
+            }
+        }
+    }
 }
 
