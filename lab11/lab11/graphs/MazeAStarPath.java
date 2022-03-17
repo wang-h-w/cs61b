12c50505d56c4e6aa6fd59bb82e729762d94fba8
Haowen Wang <wang.h.w@outlook.com>
2022-03-17 17:35:46 +0800

    complete lab11 part 3: A star algo

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,4 +1,6 @@
 package lab11.graphs;
+import java.util.Comparator;
+import edu.princeton.cs.algs4.MinPQ;
 
 /**
  *  @author Josh Hug
@@ -6,8 +8,25 @@ package lab11.graphs;
 public class MazeAStarPath extends MazeExplorer {
     private int s;
     private int t;
-    private boolean targetFound = false;
     private Maze maze;
+    private MinPQ<Node> fringe;
+
+    private class Node {
+        private int v;
+        private int priority;
+
+        Node(int v) {
+            this.v = v;
+            this.priority = distTo[v] + h(v);
+        }
+    }
+
+    private class NodeComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node v1, Node v2) {
+            return v1.priority - v2.priority;
+        }
+    }
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,22 +35,39 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        fringe = new MinPQ<>(new NodeComparator());
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
-    }
-
-    /** Finds vertex estimated to be closest to target. */
-    private int findMinimumUnmarked() {
-        return -1;
-        /* You do not have to use this method. */
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
     /** Performs an A star search from vertex s. */
-    private void astar(int s) {
-        // TODO
+    private void astar(int b) {
+        Node curr = new Node(b);
+        marked[b] = true;
+        fringe.insert(curr);
+        while (!fringe.isEmpty()) {
+            announce();
+            curr = fringe.delMin();
+            if (curr.v == t) {
+                return;
+            }
+            for (int w: maze.adj(curr.v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = curr.v;
+                    distTo[w] = distTo[curr.v] + 1;
+                    announce();
+                    if (w == t) {
+                        return;
+                    } else {
+                        fringe.insert(new Node(w));
+                    }
+                }
+            }
+        }
     }
 
     @Override
